baeadd714d20bbe3035876d317db6d1c8f86fde3
wyhong3103 <wyhong3103@gmail.com>
2022-07-17 11:10:53 +0800

    Turn board arrays into private functions

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-//DOM element
+/*
+
+    DOM element
+
+*/
 let start_player = document.querySelector(".player");
 let start_ai = document.querySelector(".ai");
 let menu = document.querySelector(".menu");
@@ -9,14 +13,19 @@ let rematch_btn = document.querySelector(".rematch-btn");
 let opp_name = document.querySelector(".opponent-stats h3");
 let opp_score = document.querySelector(".opp-cnt");
 let user_score = document.querySelector(".user-cnt");
-let d_board = [];
-let g_board = [];
 
+
+/*
+
+    MODULE
+
+*/
 let DisplayController = function(){
+    let _board = [];
     let reset_board = function(){
         //RESET CELL
         gameboard.innerHTML = "";
-        d_board = [];
+        _board = [];
         for(let i = 0; i < 3; i++){
             let temp = [];
             for(let j = 0; j < 3; j++){
@@ -25,7 +34,7 @@ let DisplayController = function(){
                 gameboard.appendChild(cell);
                 temp.push(cell);
             }
-            d_board.push(temp);
+            _board.push(temp);
         }
     }
 
@@ -33,7 +42,11 @@ let DisplayController = function(){
         game_container.style.display = 'none';
         menu.style.display = 'grid';
     };
-    return {reset_board, exit};
+
+    let getBoard = function(){
+        return _board;
+    }
+    return {reset_board, exit, getBoard};
 }();
 
 let Game = function(){
@@ -42,19 +55,22 @@ let Game = function(){
     //even = player's turn, odd = next player/ai's turn
     let turn = 0;
 
+    let _board = [];
+
     //score
     let _user_score = 0;
     let _opp_score = 0;
 
     let reset_board = function(){
-        g_board = [];
+        _board = [];
         for(let i = 0; i < 3; i++){
             let temp = [];
             for(let j = 0; j < 3; j++){
                 temp.push(-1);
             }
-            g_board.push(temp);
+            _board.push(temp);
         }
+        //reset turn
         turn = 0;
     }
 
@@ -83,30 +99,46 @@ let Game = function(){
         return [_user_score,_opp_score];
     }
 
+    let getBoard = function(){
+        return _board;
+    }
+
     let check_win = function(){
         for(let i = 0; i < 3; i++){
-            if (JSON.stringify(g_board[i]) === JSON.stringify([1,1,1])) return 1;
-            if (JSON.stringify(g_board[i]) === JSON.stringify([0,0,0])) return 0;
-            if (JSON.stringify([g_board[0][i], g_board[1][i], g_board[2][i]]) == JSON.stringify([1,1,1])) return 1;
-            if (JSON.stringify([g_board[0][i], g_board[1][i], g_board[2][i]]) == JSON.stringify([0,0,0])) return 0;
+            if (JSON.stringify(_board[i]) === JSON.stringify([1,1,1])) return 1;
+            if (JSON.stringify(_board[i]) === JSON.stringify([0,0,0])) return 0;
+            if (JSON.stringify([_board[0][i], _board[1][i], _board[2][i]]) == JSON.stringify([1,1,1])) return 1;
+            if (JSON.stringify([_board[0][i], _board[1][i], _board[2][i]]) == JSON.stringify([0,0,0])) return 0;
         }
-        if (JSON.stringify([g_board[0][0],g_board[1][1],g_board[2][2]]) == JSON.stringify([1,1,1])) return 1;
-        if (JSON.stringify([g_board[0][0],g_board[1][1],g_board[2][2]]) == JSON.stringify([0,0,0])) return 0;
-        if (JSON.stringify([g_board[2][0],g_board[1][1],g_board[0][2]]) == JSON.stringify([1,1,1])) return 1;
-        if (JSON.stringify([g_board[2][0],g_board[1][1],g_board[0][2]]) == JSON.stringify([0,0,0])) return 0;
+        if (JSON.stringify([_board[0][0],_board[1][1],_board[2][2]]) == JSON.stringify([1,1,1])) return 1;
+        if (JSON.stringify([_board[0][0],_board[1][1], _board[2][2]]) == JSON.stringify([0,0,0])) return 0;
+        if (JSON.stringify([_board[2][0],_board[1][1],_board[0][2]]) == JSON.stringify([1,1,1])) return 1;
+        if (JSON.stringify([_board[2][0],_board[1][1],_board[0][2]]) == JSON.stringify([0,0,0])) return 0;
         return -1;
     }
 
-    return {mode,getTurn, reset_board,nextTurn, exit, check_win, oppwin, userwin, getScore};
+    return {mode,getTurn,getBoard, reset_board,nextTurn, exit, check_win, oppwin, userwin, getScore};
 }();
+
+
+/*
+
+    GFNERAL FUNCTION
+
+*/
+
+
+let updateScore = function(){
+    user_score.textContent = Game.getScore()[0];
+    opp_score.textContent = Game.getScore()[1];
+}
 
 let endround = function(){
     let winner = Game.check_win();
     if (winner === -1) return;
     else if (winner === 0) Game.userwin();
-    else Game.oppwin()
-    user_score.textContent = Game.getScore()[0];
-    opp_score.textContent = Game.getScore()[1];
+    else Game.oppwin();
+	updateScore();
     init_board();
 }
 
@@ -115,7 +147,7 @@ let init_board = function(){
     Game.reset_board();
     for(let i = 0; i < 3; i++){
         for(let j = 0; j < 3; j++){
-            d_board[i][j].addEventListener('click', function(){
+            DisplayController.getBoard()[i][j].addEventListener('click', function(){
                 let r = i, c = j;
                 if (this.classList.contains("marked")) return;
                 if (Game.mode === 0){
@@ -131,7 +163,7 @@ let init_board = function(){
                         this.appendChild(img);
                     }
                 }
-                g_board[r][c] = Game.getTurn() % 2;
+                Game.getBoard()[r][c] = Game.getTurn() % 2;
                 Game.nextTurn();
                 endround();
             });
@@ -158,6 +190,7 @@ start_ai.addEventListener('click', function(){
 exit_btn.addEventListener('click', function(){
     DisplayController.exit();
     Game.exit();
+	updateScore();
 });
 
 rematch_btn.addEventListener('click', function(){
